86a4ffe368ed00f53154cab25ac10e742b10a3d8
Kiran Anand <liteninkiran@hotmail.com>
2025-10-01 14:43:58 +0100

    Initial commit

---
+++ src/app/pages/under-construction/under-construction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-under-construction',
+    imports: [],
+    templateUrl: './under-construction.component.html',
+    styleUrl: './under-construction.component.scss',
+})
+export class UnderConstructionComponent {}
