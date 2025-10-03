d14e191286e74a5114640ec789354f650cd4b600
Kiran Anand <liteninkiran@hotmail.com>
2025-10-03 19:48:52 +0100

    Implementing Angular Material Buttons

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -16,6 +16,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-pages',
@@ -28,6 +29,7 @@ import { MatIconModule } from '@angular/material/icon';
         MatToolbar,
         MatListModule,
         MatIconModule,
+        MatButtonModule,
     ],
     templateUrl: './pages.component.html',
     styleUrl: './pages.component.scss',
